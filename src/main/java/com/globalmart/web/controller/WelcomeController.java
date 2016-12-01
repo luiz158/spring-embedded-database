d59b26c5ff477923c1a0ecfdc282676d9c5883f0
Sp <551040@CTS.COM>
2016-12-01 16:05:34 +0530

    Succesfull output

--- src/main/java/com/globalmart/web/controller/WelcomeController.java
+++ src/main/java/com/globalmart/web/controller/WelcomeController.java
@@ -5,36 +5,36 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.globalmart.dao.ProductDao;
 import com.globalmart.model.Product;
 
-@Controller
+@RestController
 public class WelcomeController {
 
 	private static final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
 
 	@Autowired
-	ProductDao userDao;
+	ProductDao productDao;
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String welcome(Model model) {
+	@RequestMapping(value = "/", method = RequestMethod.GET, headers="Accept=application/json")
+	public List<Product> welcome(Model model) {
 
 		logger.debug("mkyong");
 
 		//User user = userDao.findByName("mkyong");
 		
-		List<Product> users = userDao.findAll();
+		List<Product> products = productDao.findAll();
 
-		System.out.println(users);
+		System.out.println(products);
 
-		model.addAttribute("user", users);
+		model.addAttribute("user", products);
 		
-		return "welcome";
+		return products;
 
 	}
 
